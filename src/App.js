1b5dd21f70ef6fbcf849acd3fcc80fabcb35da72
Suthiwat <suthiwat.ssirith@mail.kmutt.ac.th>
2020-03-16 15:12:42 +0700

    Add Carditem component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import './App.css';
 import Sidebar from './components/common/Sidebar'
 import Navbar from '../src/components/common/Navbar'
-  
+import Carditem from '../src/components/common/Carditem'
 function App() {
   return (
     <div>
     <Navbar/>
     <Sidebar/>
+    <Carditem/>
     </div>
   );
 }
---
+++ src/components/common/Carditem.jsx
@@ -0,0 +1,45 @@
+import React,{useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+function Carditem(){
+    const [group,setGroup] = useState([{id:1,name:"SPM system",detail:"This is group detail section"}])
+    const classes = useStyles();    
+    return (
+        <>
+        {group.map((group,index)=>(
+        <Card className={classes.root} key= {index} style = {{marginLeft:300,marginTop:80}}>
+      
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            IT60-0{group.id}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {group.detail}
+          </Typography>
+        </CardContent>
+      
+      <CardActions>
+        <Button size="small" color="primary" >
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>))}
+            
+    
+        </>
+    )
+}
+export default Carditem
