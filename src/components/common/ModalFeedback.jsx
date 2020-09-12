a1a2cc310f6657ad8723ba71a8180afde8bfc2f7
Suthiwat <suthiwat.ssirith@mail.kmutt.ac.th>
2020-09-13 01:01:27 +0700

    [ADD] feedback carousel

--- src/components/common/ModalFeedback.jsx
+++ src/components/common/ModalFeedback.jsx
@@ -1,9 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import { Modal } from "react-bootstrap"
-
+import ArrowForwardOutlinedIcon from "@material-ui/icons/ArrowForwardOutlined"
+import ArrowBackOutlinedIcon from "@material-ui/icons/ArrowBackOutlined"
 export default function ModalFeedback(props) {
+  const [counter, setCounter] = useState(0)
+
+  function nextFeedback() {
+    if (
+      counter > props.feedback.length - 1 ||
+      counter == props.feedback.length - 1
+    ) {
+      console.log("too much")
+    } else if (counter < props.feedback.length - 1) {
+      setCounter(counter + 1)
+    }
+  }
+  function prevFeedback() {
+    if (counter >= props.feedback.length - 1) {
+      setCounter(counter - 1)
+    } else if (counter == 0) {
+      console.log("already 0")
+    }
+  }
   return (
     <>
+      {console.log("counter", counter)}
+      {console.log("length", props.feedback.length)}
       <Modal
         show={props.isOpen}
         onHide={() => {
@@ -14,15 +36,26 @@ export default function ModalFeedback(props) {
         centered
       >
         <Modal.Header closeButton>
-          <Modal.Title>Feedback</Modal.Title>
+          <Modal.Title>
+            {`Feedback:   ${props.feedback[counter].author} `}
+            <ArrowBackOutlinedIcon
+            fontSize='small'
+              style={{ cursor: "pointer", backgroundColor:'#336699',borderRadius:'50%',color:'white'  }}
+              onClick={() => {
+                prevFeedback()
+              }}
+            /> &nbsp;
+            <ArrowForwardOutlinedIcon
+            fontSize='small'
+              style={{ cursor: "pointer", backgroundColor:'#336699',borderRadius:'50%',color:'white' }}
+              onClick={() => {
+                nextFeedback()
+              }}
+            />
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          Woohoo, you're reading this text in a modal! Woohoo, you're reading
-          this text in a modal!Woohoo, you're reading this text in a
-          modal!Woohoo, you're reading this text in a modal!Woohoo, you're
-          reading this text in a modal!Woohoo, you're reading this text in a
-          modal!Woohoo, you're reading this text in a modal!Woohoo, you're
-          reading this text in a modal!
+          <p className="text-break">{props.feedback[counter].detail}</p>
         </Modal.Body>
       </Modal>
     </>
