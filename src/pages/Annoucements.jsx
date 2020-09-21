74e95768f22804ea7c67c379ccb5a185defffa7e
Suthiwat <suthiwat.ssirith@mail.kmutt.ac.th>
2020-09-21 13:14:05 +0700

    [ADD] announcement  table and mocking data

--- src/pages/Annoucements.jsx
+++ src/pages/Annoucements.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState, useContext, useCallback, useEffect } from "react"
+import AnnoucementTable from "../components/common/AnnoucementTable"
+import { UserContext } from "../UserContext"
+import { Link, useParams } from "@reach/router"
+import { makeStyles } from "@material-ui/core/styles"
 
 export default function Annoucements() {
-    return (
-        <div>
-            Annoucements'page
+  const { user, setUser } = useContext(UserContext)
+  const [annoucements, setAnnoucements] = useState([
+    {
+      topic: "topic 1",
+      detail: "dsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdldsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdl"
+    },
+    {
+      topic: "topic 2",
+      detail: "dsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdl"
+    },
+    {
+      topic: "topic 3",
+      detail: "dsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdl"
+    },
+    {
+      topic: "topic 4",
+      detail: "dsfsdfsdfsdfdsfdsjfsdkljfklsdjfklsdjfklsdjlfkjsdl"
+    },
+    
+  ])
+  const [isPrefetch, setIsPreFetch] = useState(false)
+  return (
+    <>
+      {user.role === "student" && (
+        <div className="container mt-5">
+          <table
+            class="table"
+            style={{
+              outlineStyle: "solid",
+              outlineWidth: "1px",
+              outlineColor: "#C4C4C4",
+            }}
+          >
+            <thead class="thead-primary">
+              <tr>
+                <th colSpan="8" style={{ fontSize: "20px" }}>
+                  Annoucements
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {annoucements.map((annoucement, index) => (
+                <AnnoucementTable
+                  annoucement={annoucement}
+                  user={user}
+                  index={index + 1}
+                />
+              ))}
+            </tbody>
+          </table>
         </div>
-    )
+      )}
+    </>
+  )
 }
